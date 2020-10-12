109db5fb2f86a9a8125c3f0df33486fb42c36092
dogurasu <dugs134@gmail.com>
2020-10-12 14:57:41 -0700

    created ApprovalCard component

--- src/components/ApprovalCard.jsx
+++ src/components/ApprovalCard.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+    console.log(props);
     return (
         <div className="ui card">
             <div className="content">
-                {/* Are you sure? */}
-                <CommentDetail
-                    author="Tenzin"
-                    avatar={faker.image.avatar()}
-                    date="Today at 6:00PM"
-                    text="Nice Blog post!"
-                />
+                {props.children}
             </div>
             <div className="extra content">
                 <div className="ui two buttons">
